2c56328ad11e3e20365f6dc4456463fcfc2d59f3
shiyindaxiaojie <1813986321@qq.com>
2022-06-20 00:48:14 +0800

    fixed

--- eden-demo-layer-start/src/main/java/org/ylzl/eden/demo/Application.java
+++ eden-demo-layer-start/src/main/java/org/ylzl/eden/demo/Application.java
@@ -9,8 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.ylzl.eden.spring.framework.bootstrap.SpringBootApplicationTemplate;
-import org.ylzl.eden.spring.framework.cola.catchlog.annotation.EnableCatchLog;
-import org.ylzl.eden.spring.framework.error.annotation.EnableRestExceptionResolver;
+import org.ylzl.eden.spring.framework.cola.rest.autoconfigure.EnableRestExceptionResolver;
 
 /**
  * Spring Boot 引导类
@@ -19,7 +18,6 @@ import org.ylzl.eden.spring.framework.error.annotation.EnableRestExceptionResolv
  * @since 2.4.x
  */
 @EnableRestExceptionResolver
-@EnableCatchLog
 @MapperScan(basePackages = "org.ylzl.eden.demo.dao", annotationClass = Mapper.class)
 @EnableDubbo(scanBasePackages = "org.ylzl.eden.demo.service")
 @EnableDiscoveryClient
